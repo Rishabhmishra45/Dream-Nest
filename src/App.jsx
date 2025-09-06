3096e124f8f2b027a3e66a2dabd35747bac4afe0
Rishabh <rishabhmishra872660@gmail.com>
2025-09-06 23:37:54 +0530

    deploy fixing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './app.css'
+import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import Home from './components/Home/Home'
 import Login from './components/Login/Login'
